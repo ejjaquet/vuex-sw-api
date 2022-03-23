df3b20d1d00276f136a79665f0295b959e170f39
Erik-Jan Jaquet <erik-jan@feedbackcompany.com>
2022-03-23 09:10:49 +0100

    Added static files

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import VueDayjs from "vue-dayjs-plugin";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./assets/tailwind.css";
+import "./assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueDayjs);
